7a3148fbceaceb8bc4bca8cebf425dd332a5cd47
Riccardo Santarossa <brudo1968@libero.it>
2020-11-15 17:07:53 +0100

    Create classi omino e pacchetti

---
+++ Soko-ban/Soko-ban/Magazziniere.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soko_ban
+{
+    class Magazziniere
+    {
+        public Point position;
+        
+        public Magazziniere()
+        {
+            position = new Point();
+            position.X = 0;
+            position.Y = 0;
+        }
+    }
+}
---
+++ Soko-ban/Soko-ban/Pacco.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soko_ban
+{
+    enum Colore
+    {
+        rosso,
+        giallo,
+        blu,
+        verde,
+        viola
+    }
+    class Pacco
+    {
+        private Colore colore;
+        private static Random r =new Random();
+
+        public Pacco()
+        {
+            int c;
+            c = r.Next(5);
+
+            switch (c)
+            {
+                case 0:
+                    colore = Colore.rosso;
+                    break;
+                case 1:
+                    colore = Colore.giallo;
+                    break;
+                case 2:
+                    colore = Colore.blu;
+                    break;
+                case 3:
+                    colore = Colore.verde;
+                    break;
+                case 4:
+                    colore = Colore.viola;
+                    break;
+            }
+        }
+
+    }
+}
