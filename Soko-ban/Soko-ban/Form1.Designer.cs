1b6f9364ef48b575bfb392217b59436e38363ae8
Luca Loner <luca.loner@outlook.com>
2020-11-15 10:44:44 +0100

    Prova

--- Soko-ban/Soko-ban/Form1.Designer.cs
+++ Soko-ban/Soko-ban/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.btnSchiacciami = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnOpraio = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -43,16 +42,6 @@
             this.btnSchiacciami.Text = "Schiacciami";
             this.btnSchiacciami.UseVisualStyleBackColor = true;
             this.btnSchiacciami.Click += new System.EventHandler(this.btnSchiacciami_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(336, 211);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 25);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "opraio";
             // 
             // btnOpraio
             // 
@@ -70,19 +59,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnOpraio);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSchiacciami);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnSchiacciami;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnOpraio;
     }
 }
