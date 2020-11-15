cc8e27e01ba1145322350f5951452d3ae2e872e0
Luca Loner <luca.loner@outlook.com>
2020-11-15 10:28:09 +0100

    Primo commit

---
+++ Soko-ban/Soko-ban/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soko_ban
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSchiacciami_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Amiamo la russia");
+        }
+    }
+}
