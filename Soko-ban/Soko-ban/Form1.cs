256f7e25ec7eca6d3d0da6587f584e18c94447cd
Riccardo Santarossa <brudo1968@libero.it>
2020-11-19 13:00:49 +0100

    Putin non si muove, sta cadendo l'unione sovietica

--- Soko-ban/Soko-ban/Form1.cs
+++ Soko-ban/Soko-ban/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,8 +13,9 @@ namespace Soko_ban
 {
     public partial class Form1 : Form
     {
-        public int sizePacchi = 48;
+        public int sizePacchi = 48,i,j;
         private int[,] campoGioco;
+        Magazziniere m = new Magazziniere();
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +24,28 @@ namespace Soko_ban
         void drawCampoGioco()
         {
             pnlCampoGioco.Controls.Clear();
+            for ( i = 0; i < 19; i++)
+            {
+                for ( j = 0; j < 11; j++)
+                {
+                    if (campoGioco[j, i] == 1)
+                    {
+                        generaElementi("..\\..\\images\\mattoni.jpg", i, j);
+                    }
+                    else if(campoGioco[j, i]== 2)
+                    {
+                        generaElementi("..\\..\\images\\cassa.jpg", i, j);
+                    }
+                    else if(campoGioco[j,i]==3)
+                    {
+                        generaElementi("..\\..\\images\\magazziniere.jpg", i, j);
+                    }
+                       
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
             campoGioco = new int[11, 19]
             {
                 {0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//1
@@ -31,44 +55,42 @@ namespace Soko_ban
                 {0, 0, 1, 0, 0, 2, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},//5
                 {1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1},//6
                 {1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1},//7
-                {1, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},//8
+                {1, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1},//8
                 {1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1},//9
                 {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1},//10
                 {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},//11
             };
-
-            for (int i = 0; i < 19; i++)
-            {
-                for (int j = 0; j < 11; j++)
-                {
-                    if (campoGioco[j, i] == 1)
-                    {
-                        pnlCampoGioco.Size = new Size(19 * sizePacchi, 11 * sizePacchi);
-                        PictureBox pbox = new PictureBox();
-                        pbox.Image = new Bitmap(@"mattoni.jpg");
-                        pbox.SizeMode = PictureBoxSizeMode.StretchImage;
-                        pbox.Visible = true;
-                        pbox.Location = new Point(i * sizePacchi, j * sizePacchi);
-                        pbox.Size = new Size(sizePacchi, sizePacchi);
-                        pnlCampoGioco.Controls.Add(pbox);
-                    }
-                    else if(campoGioco[j, i]== 2)
-                    {
-                        pnlCampoGioco.Size = new Size(19 * sizePacchi, 11 * sizePacchi);
-                        PictureBox pbox = new PictureBox();
-                        pbox.Image = new Bitmap(@"cassa.jpg");
-                        pbox.SizeMode = PictureBoxSizeMode.StretchImage;
-                        pbox.Visible = true;
-                        pbox.Location = new Point(i * sizePacchi, j * sizePacchi);
-                        pbox.Size = new Size(sizePacchi, sizePacchi);
-                        pnlCampoGioco.Controls.Add(pbox);
-                    }
-                }
-            }
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
             drawCampoGioco();
         }
+
+       void generaElementi(string percorso, int i, int j)
+        {
+            pnlCampoGioco.Size = new Size(19 * sizePacchi, 11 * sizePacchi);
+            PictureBox pbox = new PictureBox();
+            pbox.Image = new Bitmap(percorso);
+            pbox.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbox.Visible = true;
+            pbox.Location = new Point(i * sizePacchi, j * sizePacchi);
+            pbox.Size = new Size(sizePacchi, sizePacchi);
+            pnlCampoGioco.Controls.Add(pbox);
+        }
+        
+       private void Form1_KeyDown(object sender, KeyEventArgs e)
+       {
+            switch(e.KeyCode)
+            {
+                case Keys.Left:
+                    //if (campoGioco[m.position.X , m.position.Y] != 1)
+                    //{ 
+                        campoGioco[m.position.X, m.position.Y] = 0;
+                        campoGioco[m.position.X - 1, m.position.Y] = 3;
+                        MessageBox.Show(Convert.ToString(m.position.X));
+                        drawCampoGioco();
+                    //}
+
+                    break;
+            }
+       }
     }
 }
+
