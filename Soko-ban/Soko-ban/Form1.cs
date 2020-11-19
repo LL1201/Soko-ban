fb25fe6c06aae5e228d2d1960b10b5410fee80b4
Luca Loner <luca.loner@outlook.com>
2020-11-19 16:20:58 +0100

    Inserimento classe per i dll

--- Soko-ban/Soko-ban/Form1.cs
+++ Soko-ban/Soko-ban/Form1.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Soko_ban
 {
@@ -90,6 +91,25 @@ namespace Soko_ban
                     break;
             }
        }
+
+        class DrawingControl
+        {
+            [DllImport("user32.dll")]
+            public static extern int SendMessage(IntPtr hWnd, Int32 wMsg, bool wParam, Int32 lParam);
+
+            private const int WM_SETREDRAW = 11;
+
+            public static void SuspendDrawing(Control parent)
+            {
+                SendMessage(parent.Handle, WM_SETREDRAW, false, 0);
+            }
+
+            public static void ResumeDrawing(Control parent)
+            {
+                SendMessage(parent.Handle, WM_SETREDRAW, true, 0);
+                parent.Refresh();
+            }
+        }
     }
 }
 
